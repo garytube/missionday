cbf79dda9b3495b97911d667c72436dd57a5c640
Gerald <geraldscholz88@gmail.com>
2017-07-20 21:11:16 +0200

    -gulp

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var purify = require('gulp-purifycss');
-
-gulp.task('cleanup', function() {
-  return gulp.src('assets/css/*.css')
-    .pipe(purify(['assets/**/*.js', '*.php']))
-    .pipe(gulp.dest('assets/css/app'));
-});
-
