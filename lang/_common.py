056e4601ff8b11fd7ab22b003bc6833d4cc4506e
Sergey Chizhik <s.s.chizhik@gmail.com>
2018-06-12 12:08:03 +0300

    a bit translations

--- lang/_common.py
+++ lang/_common.py
@@ -1,16 +1,8 @@
-# -*- coding: utf-8 -*-
-
-
-name = 'en'
-
 texts = {
-    'head.lang': name,
-    'head.title': 'Mission Day Dnipro',
-    'head.author': 'Dnipro',
-    'body.template': """
+    'main.text': """
         Agents, we want to discover Dnipro on 11th of August, 2018.
         In collaboration with the City of Dnipro we will provide you high quality missions to explore the city
         on foot together. Sign up to receive more information soon.
     """,
+    'main.title': 'Mission Day Dnipro'
 }
-
