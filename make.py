baaa29659a7f6b15099df9d7dadd38c3c63f6b54
Sergey Chizhik <s.s.chizhik@gmail.com>
2018-06-12 09:40:56 +0300

    make: templates + langs

--- make.py
+++ make.py
@@ -2,10 +2,21 @@
 
 from lang import ru, ua, en
 
+TEMPLATE = '_index.html'
+LANGUAGES = ['ru', 'ua', 'en']
+
+
+def process_template(lang, template):
+    return template
+
 
 def main():
-  print ('soon...')
-
+  template = open(TEMPLATE, 'r').read()
+  
+  for lang in LANGUAGES:
+    out = open('%s.html' % lang, 'w+')
+    out.write(process_template(lang, template))
+    out.close()
 
 
 if __name__ == '__main__':
